c3b1d70644305d6ca51c60b42f595911fbaa8748
JunHyung_Lee <peterbean814@gmail.com>
2022-11-08 12:32:35 +0900

    '컬럼한글화''

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,23 +9,23 @@ st.write("# 이준형 버전 테스팅")
 # 첫번째 행
 r1_col1, r1_col2, r1_col3 = st.columns(3)
 
-age = r1_col1.number_input("age", step=1, value=23)
+age = r1_col1.number_input("나이", step=1, value=23)
 
-bmi = r1_col2.number_input("bmi", value=34.40)
+bmi = r1_col2.number_input("bmi지수", value=34.40)
 
-children = r1_col3.number_input("children", step=1, value=0)
+children = r1_col3.number_input("미성년자", step=1, value=0)
 
 # 두번째 행
 r2_col1, r2_col2, r2_col3 = st.columns(3)
 
-r2_col1.write("smoker")
+r2_col1.write("흡연자")
 smoker = r2_col1.checkbox("")
 
-sex_option = ("male", "female")
-sex = r2_col2.selectbox("sex", sex_option)
+sex_option = ("남성", "여성")
+sex = r2_col2.selectbox("성별", sex_option)
 is_male = sex_option[0] == sex
 
-region_option = ('southwest', 'southeast', 'northwest', 'northeast')
+region_option = ('서구', '동구', '남구', '중구')
 region = r2_col3.selectbox("region", region_option)
 is_southwest = region_option[0] == region
 is_southeast = region_option[1] == region
